bb423fd2d9a4b7403779a78b8674cf1631969292
OrsoWojtek <ale.bru132@gmail.com>
2024-12-03 19:14:17 +0100

    Secondo commit

--- progetto/src/main/java/com/example/progetto/ILearn.java
+++ progetto/src/main/java/com/example/progetto/ILearn.java
@@ -7,12 +7,13 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class ILearn extends Application {
+
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(ILearn.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 1440, 1024);
+        stage.setTitle("ILearn");
         stage.setScene(scene);
         stage.show();
     }
